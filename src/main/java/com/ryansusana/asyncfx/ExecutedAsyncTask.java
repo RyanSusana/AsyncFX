733faf921969997a7bce845017d7ea8346c199c2
Ryan Susana <ryansusana@live.com>
2019-03-10 21:46:18 +0100

    Throw an exception if waitFor is not completed

--- src/main/java/com/ryansusana/asyncfx/ExecutedAsyncTask.java
+++ src/main/java/com/ryansusana/asyncfx/ExecutedAsyncTask.java
@@ -2,16 +2,18 @@ package com.ryansusana.asyncfx;
 
 import java.util.concurrent.TimeUnit;
 
-public class ExecutedAsyncTask<T1,  T2> {
+public class ExecutedAsyncTask<T1, T2> {
 
-    private final AsyncTask<T1,  T2> task;
+    private final AsyncTask<T1, T2> task;
 
-    public ExecutedAsyncTask(AsyncTask<T1,  T2> task) {
+    public ExecutedAsyncTask(AsyncTask<T1, T2> task) {
         this.task = task;
     }
 
     public T2 andWaitFor(long time, TimeUnit timeUnit) throws InterruptedException {
-        task.countDownLatch.await(time, timeUnit);
+        if (!task.countDownLatch.await(time, timeUnit)) {
+            throw new AsyncException(String.format("Task failed to execute within %d %s", time, timeUnit.name()));
+        }
         return task.param;
     }
 
