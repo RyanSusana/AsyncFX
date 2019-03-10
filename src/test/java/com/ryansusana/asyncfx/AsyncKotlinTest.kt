af137ac5d703a067c22741a521d584168c7c65bf
Ryan Susana <ryansusana@live.com>
2019-03-10 21:53:48 +0100

    added AsyncPool test

--- src/test/java/com/ryansusana/asyncfx/AsyncKotlinTest.kt
+++ src/test/java/com/ryansusana/asyncfx/AsyncKotlinTest.kt
@@ -1,19 +1,21 @@
 package com.ryansusana.asyncfx
 
 import com.ryansusana.asyncfx.AsyncTasks.newTask
-import javafx.embed.swing.JFXPanel
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.testfx.api.FxRobot
 import org.testfx.api.FxToolkit
 import java.util.*
+import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class AsyncKotlinTest  : FxRobot() {
+class AsyncKotlinTest : FxRobot() {
 
 
     @BeforeAll
@@ -105,5 +107,33 @@ class AsyncKotlinTest  : FxRobot() {
                 .andWait()
 
         assertTrue { atomicBoolean.get() }
+    }
+
+    @Test
+    internal fun testAsyncPool() {
+        FxToolkit.registerPrimaryStage()
+
+        val asyncTaskPool = AsyncTaskPool<Unit, Unit>()
+        val atomicInteger = AtomicInteger(0)
+        val amountOfTasks = 20
+        val durationPerTask = 100L
+
+        for (i in 1..amountOfTasks) {
+            asyncTaskPool.addTask(
+                    newTask<Unit, Unit>()
+                            .inBackground {
+                                Thread.sleep(durationPerTask)
+                                atomicInteger.getAndAdd(1)
+                            }
+                            .create()
+            )
+        }
+
+        // It needs to execute in at least half of durationPerTask * amountOfTasks.
+        // This ensures that tasks are running at the time.
+        asyncTaskPool.execute().andWaitFor((amountOfTasks / 2) * durationPerTask, TimeUnit.MILLISECONDS)
+
+        assertEquals(amountOfTasks, atomicInteger.get())
+
     }
 }
